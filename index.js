76bcca97c54fc9a999eda929d38be60e18ff63da
uharston <uharston@gmail.com>
2020-08-17 18:40:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(array, callback) {
+    return array.map(num => {
+        return callback(num)
+    })
+}
+
+function reduce(sourceArray, callback, startValue){
+    
+    let baseValue = (!!startValue) ? startValue : sourceArray[0]
+
+    for (let i = (!!startValue) ? 0 : 1; i < sourceArray.length; i++) {
+      baseValue = callback(sourceArray[i], baseValue)
+    }
+  
+    return baseValue;
+  }
+
